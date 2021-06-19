293aeaaef5cbe2b981ac7ceae67761863490b366
William Porto <william.porto@nave.rs>
2021-06-19 05:02:16 -0300

    Initial Commit

---
+++ explorer/explorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  explorerApp.swift
+//  explorer
+//
+//  Created by William Porto on 19/06/21.
+//
+
+import SwiftUI
+
+@main
+struct explorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
